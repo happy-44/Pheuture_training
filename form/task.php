239e2a8c9b850942a41d26607002de56043a7bce
happy-44 <happymittal129@gmail.com>
2022-10-07 16:52:21 +0530

    form post data :- 07/10/2022

---
+++ form/task.php
@@ -0,0 +1,15 @@
+<?php
+   print_r($_POST);
+   // echo "<br>";
+   // if(isset($_POST["register"])){
+   //    $name = (isset($_POST["username"])) ? $_POST["username"] : "value is not found! please enter your name";
+   //    $age = (isset($_POST["user_age"])) ? $_POST["user_age"] : "value is not found! please enter your age";
+   //    $email = (isset($_POST["user_email"])) ? $_POST["user_email"] : "value is not found! please enter your email";
+   //    $password = (isset($_POST["user_pass"])) ? $_POST["user_pass"] : "value is not found! please enter your password";
+   //    $gender = (isset($_POST["radio"])) ? $_POST["radio"] : "value is not found! please enter your gender";
+   //    $country = (isset($_POST["country"])) ? $_POST["country"] : "value is not found! please enter your country";
+   //    echo "<br>Name :- " .$name. "<br>Age :- " .$age. "<br>Email  :- " .$email. "<br>Password :- " .$password. "<br>Gender :- " .$gender. "<br>Country :- " .$country;
+   // }else{
+   //    echo "form is not submitting your response!!! please check!";
+   // }
+?>
---
+++ happy.php
@@ -0,0 +1,24 @@
+<?php
+    echo "hi i am happy mittal and i a good boy<br>"; 
+    $name = "happy";
+    echo $name;
+    $a = 350;
+    $b = 30;
+    $sum = $a + $b;
+    echo "<br>".$sum."<br>";
+    
+    echo "sum of numbers".$a+$b."<br>";
+    $name2 = "happymittal";
+    echo $name2;
+    
+    echo "my name is \"$name\"and age is $b<br>";
+
+    echo "my name is" .$name. "and age is ".$b."<br>";
+
+    // echo "my name is" .$name. "and age is ".$b."<br>";
+
+    var_dump($name2);
+    var_dump($sum);
+
+    if($a > $b) echo "<br>u are great";
+?>
