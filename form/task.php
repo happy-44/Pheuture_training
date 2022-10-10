005b9f3fbada11fc042281673d292734de4885f9
happy-44 <happymittal129@gmail.com>
2022-10-10 15:25:07 +0530

    post method, file operation, session-10/10/2022

--- form/task.php
+++ form/task.php
@@ -1,15 +1,54 @@
 <?php
-   print_r($_POST);
-   // echo "<br>";
-   // if(isset($_POST["register"])){
-   //    $name = (isset($_POST["username"])) ? $_POST["username"] : "value is not found! please enter your name";
-   //    $age = (isset($_POST["user_age"])) ? $_POST["user_age"] : "value is not found! please enter your age";
-   //    $email = (isset($_POST["user_email"])) ? $_POST["user_email"] : "value is not found! please enter your email";
-   //    $password = (isset($_POST["user_pass"])) ? $_POST["user_pass"] : "value is not found! please enter your password";
-   //    $gender = (isset($_POST["radio"])) ? $_POST["radio"] : "value is not found! please enter your gender";
-   //    $country = (isset($_POST["country"])) ? $_POST["country"] : "value is not found! please enter your country";
-   //    echo "<br>Name :- " .$name. "<br>Age :- " .$age. "<br>Email  :- " .$email. "<br>Password :- " .$password. "<br>Gender :- " .$gender. "<br>Country :- " .$country;
-   // }else{
-   //    echo "form is not submitting your response!!! please check!";
-   // }
+   // print_r($_POST);
+   echo "<br>";
+   if(isset($_POST["register"])){
+      $name = (isset($_POST["username"])) ? $_POST["username"] : "value is not found! please enter your name";
+      $age = (isset($_POST["user_age"])) ? $_POST["user_age"] : "value is not found! please enter your age";
+      $email = (isset($_POST["user_email"])) ? $_POST["user_email"] : "value is not found! please enter your email";
+      $password = (isset($_POST["user_pass"])) ? $_POST["user_pass"] : "value is not found! please enter your password";
+      $gender = (isset($_POST["radio"])) ? $_POST["radio"] : "value is not found! please enter your gender";
+      $country = (isset($_POST["country"])) ? $_POST["country"] : "value is not found! please enter your country";
+      // echo "<br>Name :- " .$name. "<br>Age :- " .$age. "<br>Email  :- " .$email. "<br>Password :- " .$password. "<br>Gender :- " .$gender. "<br>Country :- " .$country;
+      
+
+      // $directory = "form";
+      // $target_dir = "form/";
+      // $main_file_name = $_FILES["filename"]["name"];
+      // $main_file_name_size = $_FILES["filename"]["size"];
+      // $target_file = $target_dir . basename($main_file_name);
+      // $ext = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      
+
+      // $temp_file_name = $_FILES["filename"]["temp_name"];
+      // $target_temp_file = $target_dir . basename($temp_file_name);
+      
+      // $finalpath = $target_dir.$main_file_name;
+      // if(file_exists($finalpath)){
+      //    $file1_name = $name.date("ymd").$ext;
+      //    $finalpath = $target_dir.$name;
+      // }
+      // if(move_uploaded_file($temp_file_name,$target_dir)){
+      //    echo "file successfully uploaded";
+      // }else{
+      //    echo "unable to upload file";
+      // }
+   }else{
+      echo "form is not submitting your response!!! please check!";
+      header("refresh:3;url=index.html");
+   }
+   include_once("./table.php");
+?>
+
+
+
+<?php
+    session_start();
+    $_SESSION["username"] = "$name";
+    $_SESSION["age"] = "$age";
+    $_SESSION["email"] = "$email";
+    $_SESSION["password"] = "$password";
+    $_SESSION["gender"] = "$gender";
+    $_SESSION["country"] = "$country";
+
+    header("refresh:3;url=display.php");
 ?>
