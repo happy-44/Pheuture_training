e05ca554f8b3a5dc6311274462dc49cfe6c2b217
happy-44 <happymittal129@gmail.com>
2022-10-11 14:57:57 +0530

    curl operations using local api-11/10/2022

--- curl/display.php
+++ curl/display.php
@@ -1,12 +1,43 @@
-<?php 
+<?php
+   // print_r($_POST);
+   echo "<br>";
+//    if(isset($_POST["register"])){
+      $name = (isset($_POST["username"])) ? $_POST["username"] : "value is not found! please enter your name";
+    //   $filename = (isset($_POST["filename"])) ? $_POST["filename"] : "file not found";
+    //   $filepath = (isset($_POST["filepath"])) ? $_POST["filepath"] : "file not found";
+      $age = (isset($_POST["user_age"])) ? $_POST["user_age"] : "value is not found! please enter your age";
+      $email = (isset($_POST["user_email"])) ? $_POST["user_email"] : "value is not found! please enter your email";
+      $password = (isset($_POST["user_pass"])) ? $_POST["user_pass"] : "value is not found! please enter your password";
+      $gender = (isset($_POST["radio"])) ? $_POST["radio"] : "value is not found! please enter your gender";
+      $country = (isset($_POST["country"])) ? $_POST["country"] : "value is not found! please enter your country";
+      // echo "<br>Name :- " .$name. "<br>Age :- " .$age. "<br>Email  :- " .$email. "<br>Password :- " .$password. "<br>Gender :- " .$gender. "<br>Country :- " .$country;
+      
 
-$name = (isset($_POST["username"])) ? $_POST["username"] : "value is not found! please enter your name";
-$filename = (isset($_POST["filename"])) ? $_POST["filename"] : "file not found";
-$filepath = (isset($_POST["filepath"])) ? $_POST["filepath"] : "file not found";
-$age = (isset($_POST["user_age"])) ? $_POST["user_age"] : "value is not found! please enter your age";
-$email = (isset($_POST["user_email"])) ? $_POST["user_email"] : "value is not found! please enter your email";
-$password = (isset($_POST["user_pass"])) ? $_POST["user_pass"] : "value is not found! please enter your password";
-$gender = (isset($_POST["radio"])) ? $_POST["radio"] : "value is not found! please enter your gender";
-$country = (isset($_POST["country"])) ? $_POST["country"] : "value is not found! please enter your country";
-include_once("../form/table.php");
+//       $target_dir = "uploads/";
+//       $main_file_name = $_FILES["filename"]["name"];
+//       $main_file_name_size = $_FILES["filename"]["size"];
+//       $target_file = $target_dir . basename($main_file_name);
+//       $ext = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      
+
+//       $temp_file_name = $_FILES["filename"]["tmp_name"];
+//       $target_temp_file = $target_dir . basename($temp_file_name);
+      
+//       $finalpath = $target_dir.$main_file_name;
+//       if(file_exists($finalpath)){
+//          $main_file_name = $name.microtime(). "." .$ext;
+//          $finalpath = $target_dir.$main_file_name;
+//       } 
+//       // echo $finalpath."<br>".$temp_file_name;
+//       if(move_uploaded_file($temp_file_name,$finalpath)){
+//          echo "file successfully uploaded";
+//       }else{
+//          echo "unable to upload file";
+//       }
+//    }else{
+//       echo "form is not submitting your response!!! please check!";
+//       header("refresh:3;url=index.html");
+//    }
+   include_once("./table.php");
+
 ?>
